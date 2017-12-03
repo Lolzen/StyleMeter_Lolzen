d205199acebda4989f1c43e7b23178a1d251a1b3
Georg Eitler <funer@gmx.at>
2017-12-03 15:24:28 +0100

    layout: updates
    *fix reporting
    *make the frame attached to UIParent so it can be made invisible by
    alt+z
    *remove functions now in core
    *probably a lot more changes/fixes i don't know anymore

--- layout.lua
+++ layout.lua
@@ -1,16 +1,17 @@
 --// Layout //--
 -- Reference layout: basic layout
 
-local Lolzen = CreateFrame("Frame")
-Lolzen:SetSize(250, 80)
-Lolzen:SetPoint("BOTTOMRIGHT", UIParent, "BOTTOMRIGHT", -90, 16) --CHANGE LATER
+local Lolzen = CreateFrame("Frame", nil, UIParent)
+Lolzen:SetSize(350, 100)
+Lolzen:SetPoint("BOTTOMRIGHT", UIParent, "BOTTOMRIGHT", -10, 10) --CHANGE LATER
 
 Lolzen:EnableMouse(true)
 Lolzen:SetMovable(true)
 Lolzen:SetUserPlaced(true)
+StyleMeter.LayoutFrame = Lolzen
 
 -- Script for moving the frame and resetting data
-Lolzen:SetScript("OnMouseDown", function(self, button)
+local registerClicks = function(self, button)
 	if IsAltKeyDown() then
 		Lolzen:ClearAllPoints()
 		Lolzen:StartMoving()
@@ -22,21 +23,34 @@ Lolzen:SetScript("OnMouseDown", function(self, button)
 			print("|cff5599ffStyleMeter:|r Data has been reset.")
 		end
 	end
-end)
+	-- Reporting data from context menu
+	if button == "RightButton" then
+		-- Disable report spells used to function when not clicking statusbars
+		if self:GetName() ~= nil then
+			ToggleDropDownMenu(1, nil, StyleMeter.Lolzen_DropDownMenu, self:GetName(), 0, 0)
+			StyleMeter.clicked = tonumber(string.match(self:GetName(), "%d"))
+		else
+			ToggleDropDownMenu(1, nil, StyleMeter.Lolzen_DropDownMenu, StyleMeter.LayoutFrame, 0, 0)
+			StyleMeter.clicked = 0
+		end
+	end
+end
+Lolzen:SetScript("OnMouseDown", registerClicks)
 
 Lolzen:SetScript("OnMouseUp", function()
 	Lolzen:StopMovingOrSizing()
 end)
 
 -- Script for fake-scrolling
-local viewrange = 1
+local viewrange = 0
 Lolzen:SetScript("OnMouseWheel", function(self, direction)
 	if direction == 1 then -- "up"
-		if viewrange > 1 then
+		if viewrange > 0 then
 			viewrange = viewrange - 1
 		end
 	elseif direction == -1 then -- "down"
-		if viewrange < #StyleMeter.DB.rank then
+	--	print(#StyleMeter.DB.rank)
+		if viewrange +5 < #StyleMeter.DB.rank then
 			viewrange = viewrange + 1
 		end
 	end
@@ -65,13 +79,13 @@ for i=1, 5, 1 do
 	-- Create the StatusBars
 	if not sb[i] then
 		sb[i] = CreateFrame("StatusBar", "StatusBar"..i, Lolzen)
-		sb[i]:SetHeight(13)
-		sb[i]:SetWidth(Lolzen:GetWidth() -8)
+		sb[i]:SetHeight(15)
+		sb[i]:SetWidth(Lolzen:GetWidth() -12)
 		sb[i]:SetStatusBarTexture("Interface\\AddOns\\StyleMeter_Lolzen\\Textures\\statusbar")
 		if i == 1 then
-			sb[i]:SetPoint("TOPLEFT", Lolzen, 4, -4)
+			sb[i]:SetPoint("TOPLEFT", Lolzen, 6, -7)
 		elseif i >= 1 and i <= 5 then
-			sb[i]:SetPoint("TOP", sb[i-1], "BOTTOM", 0, -2)
+			sb[i]:SetPoint("TOP", sb[i-1], "BOTTOM", 0, -3)
 		end
 	end
 
@@ -99,40 +113,39 @@ for i=1, 5, 1 do
 	if not sb[i].string1 then
 		-- #. Name
 		sb[i].string1 = sb[i]:CreateFontString(nil, "OVERLAY")
-		sb[i].string1:SetFont("Fonts\\FRIZQT__.TTF", 10, "OUTLINE")
-		sb[i].string1:SetPoint("TOPLEFT", sb[i], "TOPLEFT", 2, -1.5)
+		sb[i].string1:SetFont("Fonts\\FRIZQT__.TTF", 12, "OUTLINE")
+		sb[i].string1:SetPoint("TOPLEFT", sb[i], "TOPLEFT", 2, -0.5)
 	end
 	if not sb[i].string2 then
 		-- modevalue (modevalue%)
 		sb[i].string2 = sb[i]:CreateFontString(nil, "OVERLAY")
-		sb[i].string2:SetFont("Fonts\\FRIZQT__.TTF", 10, "OUTLINE")
-		sb[i].string2:SetPoint("TOPRIGHT", sb[i], "TOPRIGHT", -2, -1.5)
-	end
-
-	-- More details on hovering the Statusbars
-	if not sb[i].content then
-		sb[i].content = {}
+		sb[i].string2:SetFont("Fonts\\FRIZQT__.TTF", 12, "OUTLINE")
+		sb[i].string2:SetPoint("TOPRIGHT", sb[i], "TOPRIGHT", -2, -0.5)
 	end
 
 	sb[i]:SetScript("OnEnter", function(self)
-		local curModeVal = StyleMeter.moduleDB[StyleMeter.activeModule][StyleMeter.DB.rank[viewrange + i - 1]]
+		local curModeVal, curModeTotal = StyleMeter.getModeData(viewrange + i)
+		local curModeCombatTime, curModeSpells = StyleMeter.getTimeAndSpells(viewrange + i)
 		if curModeVal and curModeVal > 0 then
 			local sortedSpells = {}
-			-- Sort Spells & Abilities by active mode, so they aren't getting displayed funny
 			GameTooltip:SetOwner(sb[i], "ANCHOR_TOPLEFT", 0, 0)
-			GameTooltip:AddDoubleLine(StyleMeter.DB.rank[viewrange + i - 1], StyleMeter.DB.players[StyleMeter.DB.rank[viewrange + i - 1]].class, 1, 1, 1, 1, 1, 1)
-			GameTooltip:AddDoubleLine("Total", curModeVal.." ("..(curModeVal / StyleMeter.moduleDBtotal[StyleMeter.activeModule] * 100).."%)" or "", 85/255, 153/255, 255/255, 1, 1, 1)
-			GameTooltip:AddDoubleLine(StyleMeter.activeModule.." per Second:", StyleMeter.siValue(curModeVal / StyleMeter.DB.players[StyleMeter.DB.rank[viewrange + i - 1]][StyleMeter.activeModule].combatTime),85/255, 153/255, 255/255, 1, 1, 1)
-			GameTooltip:AddLine(" ")
-			GameTooltip:AddLine("Spells/Abilities used", 85/255, 153/255, 255/255)
-			for k, v in pairs(StyleMeter.DB.spells[StyleMeter.activeModule][StyleMeter.DB.rank[viewrange + i - 1]]) do
-				-- Instert spellnames to sortedSpells
-				tinsert(sortedSpells, k)
-				-- Sort the spells
-				sort(sortedSpells, function(a, b) return StyleMeter.DB.spells[StyleMeter.activeModule][StyleMeter.DB.rank[viewrange + i - 1]][a] > StyleMeter.DB.spells[StyleMeter.activeModule][StyleMeter.DB.rank[viewrange + i - 1]][b] end)
-			end
-			for _, v in pairs(sortedSpells) do
-				GameTooltip:AddDoubleLine(v, StyleMeter.DB.spells[StyleMeter.activeModule][StyleMeter.DB.rank[viewrange + i - 1]][v]..format(" (%.0f%%)", StyleMeter.DB.spells[StyleMeter.activeModule][StyleMeter.DB.rank[viewrange + i - 1]][v] / (StyleMeter.moduleDB[StyleMeter.activeModule][StyleMeter.DB.rank[viewrange + i - 1]]) * 100), 1, 1, 1, 1, 1, 1)
+			GameTooltip:AddDoubleLine(StyleMeter.DB.rank[viewrange + i], StyleMeter.DB.players[StyleMeter.DB.rank[viewrange + i]].class, 1, 1, 1, 1, 1, 1)
+			GameTooltip:AddDoubleLine("Total", curModeVal.." ("..(curModeVal / curModeTotal * 100).."%)" or "", 85/255, 153/255, 255/255, 1, 1, 1)
+			if not StyleMeter.module[StyleMeter.activeModule].limitdata == true then
+				GameTooltip:AddDoubleLine(StyleMeter.activeModule.." per Second:", StyleMeter.siValue(curModeVal / curModeCombatTime),85/255, 153/255, 255/255, 1, 1, 1)
+				GameTooltip:AddLine(" ")
+				GameTooltip:AddLine("Spells/Abilities used", 85/255, 153/255, 255/255)
+				for k, v in pairs(curModeSpells) do
+					-- Instert spellnames to sortedSpells
+					if not sortedSpells[k] then
+						tinsert(sortedSpells, k)
+					end
+					-- Sort the spells
+					sort(sortedSpells, function(a, b) return curModeSpells[a].amount > curModeSpells[b].amount end)
+				end
+				for _, v in pairs(sortedSpells) do
+					GameTooltip:AddDoubleLine(v, curModeSpells[v].amount..format(" (%.0f%%)", curModeSpells[v].amount / curModeVal * 100), 1, 1, 1, 1, 1, 1)
+				end
 			end
 			GameTooltip:Show()
 		end
@@ -142,51 +155,16 @@ for i=1, 5, 1 do
 		GameTooltip:Hide() 
 	end)
 
-	-- Script for moving the frame and resetting data
-	-- duplicate, because we just want to click without precision; otherwise we'd have to click exactly on the "Lolzen" frame
-	sb[i]:SetScript("OnMouseDown", function(self, button)
-		if IsAltKeyDown() then
-			Lolzen:ClearAllPoints()
-			Lolzen:StartMoving()
-		end
-		if IsShiftKeyDown() then
-			if button == "LeftButton" then
-				StyleMeter.resetData()
-				StyleMeter.UpdateLayout()
-				print("|cff5599ffStyleMeter:|r Data has been reset.")
-			end
-		end
-		-- Reporting data from context menu
-		if button == "RightButton" then
-			ToggleDropDownMenu(1, nil, StyleMeter.Lolzen_DropDownMenu, sb[i], 0, 0)
-			StyleMeter.clicked = i
-		end
-	end)
+	-- Register sb[i] for click events
+	sb[i]:SetScript("OnMouseDown", registerClicks)
 	sb[i]:SetScript("OnMouseUp", function()
 		Lolzen:StopMovingOrSizing()
 	end)
 end
 
---//#Functions called by core#//--
-
-function StyleMeter.layoutSpecificReset()
-	for i=1, 5, 1 do
-		for k, v in pairs(sb[i].content) do
-			sb[i].content[v] = ""
-		end
-	end
-end
-
-function StyleMeter.switchMode(mode)
-	if mode ~= nil then
-		StyleMeter.activeModule = mode
-	end
-
-	-- Sort Statusbars by active mode, so they aren't getting displayed funny
-	if StyleMeter.moduleDB[StyleMeter.activeModule] then
-		sort(StyleMeter.DB.rank, function(a,b) return (StyleMeter.moduleDB[StyleMeter.activeModule][a] or 0) > (StyleMeter.moduleDB[StyleMeter.activeModule][b] or 0) end)
-	end
-
+--//#Layout Functions#//--
+-- hook into switchModule to colorize the statusbars
+hooksecurefunc(StyleMeter, "switchModule", function(self)
 	for i=1, 5, 1 do
 		if StyleMeter.activeModule == "Damage" or StyleMeter.activeModule == "Damage Taken" then
 			sb[i]:SetStatusBarColor(0.8, 0, 0)
@@ -199,29 +177,28 @@ function StyleMeter.switchMode(mode)
 		else
 			sb[i]:SetStatusBarColor(0.7, 0.7, 0.7)
 		end
-	end
-end
-
-function StyleMeter.UpdateLogin()
-	-- Update on login and select Damage as teh Standard mode
-	-- TODO: make this worked with saved vars, so next time it'll be the last set mode
-	StyleMeter.switchMode("Damage")
-end
+	end 
+end)
 
 function StyleMeter.UpdateLayout()
 	for i=1, 5, 1 do
-		local curModeVal = StyleMeter.moduleDB[StyleMeter.activeModule][StyleMeter.DB.rank[viewrange + i - 1]]
+		local curModeVal, curModeTotal = StyleMeter.getModeData(viewrange + i)
+		local curModeCombatTime, curModeSpells = StyleMeter.getTimeAndSpells(viewrange + i)
 		if curModeVal and curModeVal > 0 then
 			if sb[i]:GetAlpha() == 0 then
 				sb[i]:SetAlpha(1)
+				sb[i].border:Show()
+				sb[i].bg:Show()
 			end
-			sb[i]:SetMinMaxValues(0, StyleMeter.moduleDB[StyleMeter.activeModule][StyleMeter.DB.rank[1]] or 0)
+			sb[i]:SetMinMaxValues(0, StyleMeter.getModeData(1))
 			sb[i]:SetValue(curModeVal)
 			-- Strings
-			sb[i].string2:SetFormattedText("%s (%.0f%%)", StyleMeter.siValue(curModeVal), curModeVal / StyleMeter.moduleDBtotal[StyleMeter.activeModule] * 100)
-			sb[i].string1:SetFormattedText("%d.  |cff%02x%02x%02x%s|r", viewrange + i - 1, StyleMeter.DB.players[StyleMeter.DB.rank[viewrange + i - 1]].classcolor.r*255, StyleMeter.DB.players[StyleMeter.DB.rank[viewrange + i - 1]].classcolor.g*255, StyleMeter.DB.players[StyleMeter.DB.rank[viewrange + i - 1]].classcolor.b*255, StyleMeter.DB.rank[viewrange + i - 1])
-			sb[i].border:Show()
-			sb[i].bg:Show()
+			if StyleMeter.module[StyleMeter.activeModule].limitdata == true then
+				sb[i].string2:SetFormattedText("%s", StyleMeter.siValue(curModeVal))
+			else
+				sb[i].string2:SetFormattedText("%s [%s] (%.0f%%)", StyleMeter.siValue(curModeVal), StyleMeter.siValue(curModeVal / curModeCombatTime).." "..string.sub(StyleMeter.activeModule, 1, 1).."ps", curModeVal / curModeTotal * 100)
+			end
+			sb[i].string1:SetFormattedText("%d.  |cff%02x%02x%02x%s|r", viewrange + i, StyleMeter.DB.players[StyleMeter.DB.rank[viewrange + i]].classcolor.r*255, StyleMeter.DB.players[StyleMeter.DB.rank[viewrange + i]].classcolor.g*255, StyleMeter.DB.players[StyleMeter.DB.rank[viewrange + i]].classcolor.b*255, StyleMeter.DB.rank[viewrange + i])
 		else
 			if sb[i]:GetAlpha() == 1 then
 				sb[i]:SetAlpha(0)
@@ -229,13 +206,10 @@ function StyleMeter.UpdateLayout()
 				sb[i].string2:SetText(nil)
 				sb[i].border:Hide()
 				sb[i].bg:Hide()
-				sb[i].content = {}
 			end
 		end
 	end
 
-	-- Sort Statusbars by active mode, so they aren't getting displayed funny
-	if StyleMeter.moduleDB[StyleMeter.activeModule] then
-		sort(StyleMeter.DB.rank, function(a,b) return (StyleMeter.moduleDB[StyleMeter.activeModule][a] or 0) > (StyleMeter.moduleDB[StyleMeter.activeModule][b] or 0) end)
-	end
+	-- Sort Statusbars
+	StyleMeter.sortRank()
 end
